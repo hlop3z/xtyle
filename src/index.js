ca8fafd28cea570fc8b75d27deb2d015eeaee502
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-10-18 20:12:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 function getStyle(id) {
-    const attr = 'data-design';
-    const found = document.querySelectorAll(`[${attr}="${id}"]`);
-    if (found.length > 0) {
-        return found[0];
-    } else {
-        const style = document.createElement('style');
-        style.setAttribute(attr, id);
-        document.head.append(style);
-        return style;
-    }
+  const attr = "data-design";
+  const found = document.querySelectorAll(`[${attr}="${id}"]`);
+  if (found.length > 0) {
+    return found[0];
+  } else {
+    const style = document.createElement("style");
+    style.setAttribute(attr, id);
+    document.head.append(style);
+    return style;
+  }
 }
 
+const removeSpace = (text) =>
+  text
+    .replace(/\s\s+/g, " ")
+    .replace(/\r?\n|\r/g, "")
+    .trim();
+
 /**
- * Utility function to add replaceable CSS.
+ * Add replaceable CSS.
  * @param {string} styleID
  * @param {string} styleString
  */
 function addStyle(id, styleString) {
-    const style = getStyle(id);
-    style.textContent = styleString;
+  const style = getStyle(id);
+  style.textContent = removeSpace(styleString);
 }
 
 export default addStyle;
