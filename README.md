ed5f0e7d237378cabd85ad44fa086217d1a9aa15
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-12-29 18:19:25 -0600

    added util for html, css and js

--- README.md
+++ README.md
@@ -12,9 +12,38 @@ npm install xtyle
 
 ```js
 import xtyle from "xtyle";
+
+/* HTML & CSS */
 /**
- * @param {string} UniqueID
- * @param {string} styleString
-**/
-xtyle('app-one', '* { margin: 0; padding: 0; }')
+ * @param {string} id: UniqueID
+ * @param {string} code: codeString
+ **/
+
+/* JavaScript */
+/**
+ * @param {string} id: UniqueID
+ * @param {string} src: source
+ **/
+
+xtyle("app-one", "* { margin: 0; padding: 0; }");
+
+xtyle.css({
+  id: "main-style",
+  code: "* { background-color: pink }",
+});
+
+xtyle.js({
+  id: "main-script",
+  src: "/first.js",
+});
+
+xtyle.html({
+  id: "single-via-ID",
+  code: "My Application",
+});
+
+xtyle.html({
+  // Dynamic (No - ID)
+  code: "<div>Add as Many as you Need.<div>",
+});
 ```
--- src/index.js
+++ src/index.js
@@ -1,30 +1,89 @@
-function getStyle(id) {
-  const attr = "data-design";
-  const found = document.querySelectorAll(`[${attr}="${id}"]`);
-  if (found.length > 0) {
-    return found[0];
+const prefixCSS = "gui-design";
+const prefixJavaScript = "gui-script";
+
+function injectCSS(props) {
+  /**
+   * Inject replaceable CSS.
+   * @param {string} id
+   * @param {string} code
+   */
+  const ID = props.id;
+  const stringTemplate = props.code;
+
+  function getStyle(id) {
+    /**
+     * Add replaceable CSS.
+     * @param {string} styleID
+     * @param {string} styleString
+     */
+    const found = window.document.querySelectorAll(`[${prefixCSS}="${id}"]`);
+    if (found.length > 0) {
+      return found[0];
+    } else {
+      const style = window.document.createElement("style");
+      style.setAttribute(prefixCSS, id);
+      window.document.head.append(style);
+      return style;
+    }
+  }
+
+  function removeSpace(text) {
+    /**
+     * Clean CSS Code.
+     * @param {string} text
+     */
+    return text
+      .replace(/\s\s+/g, " ")
+      .replace(/\r?\n|\r/g, "")
+      .trim();
+  }
+
+  const elem = getStyle(ID);
+  elem.textContent = removeSpace(stringTemplate);
+}
+
+function injectHTML(props) {
+  /**
+   * Inject replaceable HTML.
+   * @param {string} id
+   * @param {string} code
+   */
+  const ID = props.id;
+  const stringTemplate = props.code;
+  if (ID) {
+    window.document.querySelector(`#${ID}`).innerHTML = stringTemplate;
   } else {
-    const style = document.createElement("style");
-    style.setAttribute(attr, id);
-    document.head.append(style);
-    return style;
+    const elem = document.createElement("div");
+    elem.innerHTML = stringTemplate;
+    document.body.insertBefore(elem, document.body.firstChild);
   }
 }
 
-const removeSpace = (text) =>
-  text
-    .replace(/\s\s+/g, " ")
-    .replace(/\r?\n|\r/g, "")
-    .trim();
+function injectJavaScript(props) {
+  /**
+   * Inject replaceable HTML.
+   * @param {string} id
+   * @param {string} src
+   */
 
-/**
- * Add replaceable CSS.
- * @param {string} styleID
- * @param {string} styleString
- */
-function addStyle(id, styleString) {
-  const style = getStyle(id);
-  style.textContent = removeSpace(styleString);
+  const ID = props.id;
+  const SRC = props.src;
+  if (SRC) {
+    let elem = document.querySelectorAll(`[${prefixJavaScript}="${ID}"]`);
+    if (elem.length > 0) {
+      elem[0].remove();
+    }
+    elem = window.document.createElement("script");
+    elem.setAttribute(prefixJavaScript, ID);
+    // Inject SRC.js
+    elem.src = SRC;
+    const firstScript = window.document.getElementsByTagName("script")[0];
+    firstScript.parentNode.insertBefore(elem, firstScript);
+  }
 }
 
-export default addStyle;
+export default {
+  html: injectHTML,
+  css: injectCSS,
+  js: injectJavaScript,
+};
