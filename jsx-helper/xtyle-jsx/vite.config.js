c24f859f10b21e1cdec08bdec1274f653e633736
hlop3z <23062270+hlop3z@users.noreply.github.com>
2023-02-04 00:42:57 -0600

    tools

--- jsx-helper/xtyle-jsx/vite.config.js
+++ jsx-helper/xtyle-jsx/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@tool": fileURLToPath(new URL("./tools", import.meta.url)),
     },
   },
 });
