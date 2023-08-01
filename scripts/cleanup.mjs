520bc3d7deb5b04c0d614885f2e66f727b082be8
hlop3z <23062270+hlop3z@users.noreply.github.com>
2023-08-01 18:51:34 -0500

    v2.1.6

--- scripts/cleanup.mjs
+++ scripts/cleanup.mjs
@@ -1,19 +1,8 @@
-import fs from "fs";
 import { execSync } from "child_process";
-import ProjectName from "./__init__.mjs";
-
-const sourceCSS = "dist/style.css";
-const destinationCSS = `dist/${ProjectName}.min.css`;
-
-try {
-  fs.copyFileSync(sourceCSS, destinationCSS);
-} catch (error) {
-  console.error("Error executing command:", error.message);
-}
 
 try {
   const result = execSync(
-    "rm dist/index.js dist/style.css dist/vite.svg dist/preact.min.js dist/xtyle.min.js"
+    "rm dist/index.js dist/vite.svg dist/preact.min.js dist/xtyle.min.js"
   );
   console.log(result.toString());
 } catch (error) {
